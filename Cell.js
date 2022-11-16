64520175dffaeb0a53ca58507c50d08c26cf6b24
Saul <phasecross@hotmail.com>
2022-11-16 12:19:48 -0700

    Fixed file paths, added p5js CDN, and formatted code

--- Cell.js
+++ Cell.js
@@ -1,4 +1,5 @@
-function Cell(i, j, scl) {
+class Cell {
+  constructor(i, j, scl) {
     this.i = i;
     this.j = j;
     this.scl = scl;
@@ -10,119 +11,123 @@ function Cell(i, j, scl) {
     this.revealed = false;
     this.flagged = false;
 
-    this.show = function() {
-        stroke(2);
-        strokeWeight(2);
-        if (this.flagged && !this.revealed) {
-            fill(225, 20, 0);
-        } else if (!this.revealed) {
-            fill(155);
-        }
+    this.show = function () {
+      stroke(2);
+      strokeWeight(2);
+      if (this.flagged && !this.revealed) {
+        fill(225, 20, 0);
+      } else if (!this.revealed) {
+        fill(155);
+      }
 
+      rect(this.x, this.y, scl, scl);
 
+      if (this.toReveal && !this.flagged) {
+        fill(200);
         rect(this.x, this.y, scl, scl);
+        this.revealed = true;
 
-        if (this.toReveal && !this.flagged) {
-
-            fill(200);
-            rect(this.x, this.y, scl, scl);
-            this.revealed = true;
-
-            if (this.mine) {
-                noStroke();
-                fill(25);
-                ellipse(this.x + (this.scl / 2), this.y + (this.scl / 2), scl / 2);
-            } else {
-                if (this.minesAround !== 0) {
-                    fill(0);
-                    textSize(17);
-                    strokeWeight(1);
-                    textAlign(CENTER, CENTER);
-                    text(this.minesAround, this.x + (scl / 2), this.y + (scl / 2) + 2);
-                } else {
-                    for (let xOff = -1; xOff <= 1; xOff++) {
-                        for (let yOff = -1; yOff <= 1; yOff++) {
-                            let k = this.i + xOff;
-                            let l = this.j + yOff;
-
-                            if (k > -1 && k < cols && l > -1 && l < rows) {
-                                grid[k][l].reveal();
-                            }
-                        }
-                    }
-                }
-            }
-        } else {
-            this.toReveal = false;
-        }
-    };
-
-    this.contains = function(x, y) {
-        return (x > this.x && x < this.x + this.scl && y > this.y && y < this.y + this.scl)
-    };
-
-    this.reveal = function() {
-        this.toReveal = true;
-    };
-
-    this.countMinesAround = function() {
         if (this.mine) {
-            return -1;
-        }
-
-        let total = 0;
-        for (let xOff = -1; xOff <= 1; xOff++) {
-            for (let yOff = -1; yOff <= 1; yOff++) {
+          noStroke();
+          fill(25);
+          ellipse(this.x + this.scl / 2, this.y + this.scl / 2, scl / 2);
+        } else {
+          if (this.minesAround !== 0) {
+            fill(0);
+            textSize(17);
+            strokeWeight(1);
+            textAlign(CENTER, CENTER);
+            text(this.minesAround, this.x + scl / 2, this.y + scl / 2 + 2);
+          } else {
+            for (let xOff = -1; xOff <= 1; xOff++) {
+              for (let yOff = -1; yOff <= 1; yOff++) {
                 let k = this.i + xOff;
                 let l = this.j + yOff;
 
                 if (k > -1 && k < cols && l > -1 && l < rows) {
-                    let neighbor = grid[k][l];
-                    if (neighbor.mine) {
-                        total++;
-                    }
+                  grid[k][l].reveal();
                 }
+              }
             }
+          }
+        }
+      } else {
+        this.toReveal = false;
+      }
+    };
+
+    this.contains = function (x, y) {
+      return (
+        x > this.x &&
+        x < this.x + this.scl &&
+        y > this.y &&
+        y < this.y + this.scl
+      );
+    };
+
+    this.reveal = function () {
+      this.toReveal = true;
+    };
+
+    this.countMinesAround = function () {
+      if (this.mine) {
+        return -1;
+      }
+
+      let total = 0;
+      for (let xOff = -1; xOff <= 1; xOff++) {
+        for (let yOff = -1; yOff <= 1; yOff++) {
+          let k = this.i + xOff;
+          let l = this.j + yOff;
+
+          if (k > -1 && k < cols && l > -1 && l < rows) {
+            let neighbor = grid[k][l];
+            if (neighbor.mine) {
+              total++;
+            }
+          }
+        }
+      }
+
+      this.minesAround = total;
+    };
+
+    this.flag = function () {
+      this.flagged = !this.flagged;
+    };
+
+    this.revealAround = function () {
+      if (this.revealed) {
+        let flaggedAround = 0;
+
+        for (let xOff = -1; xOff <= 1; xOff++) {
+          for (let yOff = -1; yOff <= 1; yOff++) {
+            let k = this.i + xOff;
+            let l = this.j + yOff;
+
+            if (k > -1 && k < cols && l > -1 && l < rows) {
+              if (grid[k][l].flagged) {
+                flaggedAround++;
+              }
+            }
+          }
         }
 
-        this.minesAround = total;
-    };
+        if (flaggedAround === this.minesAround) {
+          for (let xOff = -1; xOff <= 1; xOff++) {
+            for (let yOff = -1; yOff <= 1; yOff++) {
+              let k = this.i + xOff;
+              let l = this.j + yOff;
 
-    this.flag = function() {
-        this.flagged = !this.flagged;
-    };
-
-    this.revealAround = function() {
-        if (this.revealed) {
-            let flaggedAround = 0;
-
-            for (let xOff = -1; xOff <= 1; xOff++) {
-                for (let yOff = -1; yOff <= 1; yOff++) {
-                    let k = this.i + xOff;
-                    let l = this.j + yOff;
-
-                    if (k > -1 && k < cols && l > -1 && l < rows) {
-                        if (grid[k][l].flagged) {
-                            flaggedAround++;
-                        }
-                    }
+              if (k > -1 && k < cols && l > -1 && l < rows) {
+                if (!grid[k][l].flagged) {
+                  grid[k][l].reveal();
                 }
+              }
             }
-
-            if (flaggedAround === this.minesAround) {
-                for (let xOff = -1; xOff <= 1; xOff++) {
-                    for (let yOff = -1; yOff <= 1; yOff++) {
-                        let k = this.i + xOff;
-                        let l = this.j + yOff;
-
-                        if (k > -1 && k < cols && l > -1 && l < rows) {
-                            if (!grid[k][l].flagged) {
-                                grid[k][l].reveal();
-                            }
-                        }
-                    }
-                }
-            }
+          }
         }
+      }
     };
+  }
 }
